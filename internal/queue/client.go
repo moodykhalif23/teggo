9fa6f8685a962d89950bda6669e63232ae81c7d4
moodykhalif23 <b.wayatg7@gmail.com>
2026-06-02 14:32:40 +0300

    real bus sol

--- internal/queue/client.go
+++ internal/queue/client.go
@@ -1,6 +1,7 @@
 package queue
 
 import (
+	"github.com/jackc/pgx/v5"
 	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/riverqueue/river"
 	"github.com/riverqueue/river/riverdriver/riverpgxv5"
@@ -8,8 +9,7 @@ import (
 	"b2bcommerce/internal/queue/jobs"
 )
 
-// NewWorkerClient builds a river client that processes jobs (used by cmd/worker).
-func NewWorkerClient(pool *pgxpool.Pool) (*river.Client[*pgxpool.Pool], error) {
+func NewWorkerClient(pool *pgxpool.Pool) (*river.Client[pgx.Tx], error) {
 	workers := river.NewWorkers()
 	river.AddWorker(workers, &jobs.SendEmailWorker{})
 	// Register additional workers here as modules add jobs.
@@ -23,6 +23,6 @@ func NewWorkerClient(pool *pgxpool.Pool) (*river.Client[*pgxpool.Pool], error) {
 }
 
 // NewInsertClient builds an insert-only river client (used by the API to enqueue).
-func NewInsertClient(pool *pgxpool.Pool) (*river.Client[*pgxpool.Pool], error) {
+func NewInsertClient(pool *pgxpool.Pool) (*river.Client[pgx.Tx], error) {
 	return river.NewClient(riverpgxv5.New(pool), &river.Config{})
 }
